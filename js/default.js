b7afe5ddc2c7b56b4faf500e64d1376a5fbeb740
techpool <suryadeep10@gmail.com>
2016-03-01 20:41:58 +0530

    pot loader added

--- js/default.js
+++ js/default.js
@@ -1,4 +1,34 @@
 $('main').hide();
+
+var xmlns = "http://www.w3.org/2000/svg",
+  select = function(s) {
+    return document.querySelector(s);
+  },
+  selectAll = function(s) {
+    return document.querySelectorAll(s);
+  },
+  container = select('.bottle_loader')
+
+//center the container cos it's pretty an' that
+TweenMax.set(container, {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  xPercent: -50,
+  yPercent: -50
+})
+TweenMax.set('svg', {
+  visibility: 'visible'
+})
+
+var tl = new TimelineMax();
+tl.staggerTo('#bubbleGroup circle', 3, {
+  attr: {
+    cy: 200
+  },
+  repeat: -1
+}, 0.6)
+
 $(window).load(function() {
      $('#gradient').hide();
      $('main').show();
